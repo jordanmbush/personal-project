c42f6d1f992a0ef29be1d45fa1a812826d2d26e3
unknown <jordanmbush@gmail.com>
2018-08-04 09:16:21 -0700

    setup sass

--- src/components/atoms/buttons/button.jsx
+++ src/components/atoms/buttons/button.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './button.css';
 
 const Button = ({ children, onClick, className }) => (
   <button type="button" onClick={onClick} className={`basic-btn ${className}`}>
